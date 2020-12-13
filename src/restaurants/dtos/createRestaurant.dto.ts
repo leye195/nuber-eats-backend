42eae81647551fc5f6d9cbc8453a171f90a2d2b8
leye195 <leye195@naver.com>
2020-12-13 16:05:21 +0900

    feat: add class-validator, class-transformer

--- src/restaurants/dtos/createRestaurant.dto.ts
+++ src/restaurants/dtos/createRestaurant.dto.ts
@@ -1,17 +1,23 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 // @InputType() //하나의 object를 argument로서 graphql에 전달하기 위한 용도로 활용
 @ArgsType() // 분리된 값들을 Graphql argument로 전달해 줄 수 있도록 해줌
 export class CreateRestaurantDTO {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
 
   @Field(() => String)
+  @IsString()
   address: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
