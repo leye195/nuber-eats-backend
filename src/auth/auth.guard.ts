57a3dffc7612c016c62623d2e2eed062983bed50
leye195 <leye195@naver.com>
2021-02-07 19:12:28 +0900

    feat: get meta data through reflactor class

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,13 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-
+import { AllowedRoles } from './role.decorator';
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflactor: Reflector) {}
   canActivate(context: ExecutionContext) {
+    const roles = this.reflactor.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+
+    if (!roles) {
+      return true;
+    }
+
     //convert context to gqlContext
     const gqlContext = GqlExecutionContext.create(context).getContext();
     const user = gqlContext['user'];
     if (!user) return false;
-    return true;
+
+    return roles.includes(user.role) || roles.includes('Any');
   }
 }
